5ea4917885c6958ef7bc042d8b0a9d6c61f55dd9
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2023-06-04 14:39:36 -0400

    Made change

--- Python Projects/samplePython.py
+++ Python Projects/samplePython.py
@@ -9,5 +9,7 @@ def sampleFunction(num1, num2):
 #Main Script
 if __name__ == "__main__":
     sampleNum = sampleFunction(10, 10)
+    anotherNum = sampleFunction(100, 1)
     print(sampleNum)
+    print(anotherNum)
     
